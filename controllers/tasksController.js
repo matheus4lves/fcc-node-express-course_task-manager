bb48cbf718441cb1f0afa00747a5ede1ce317db8
Matheus Alves <matheusalves.bmd@gmail.com>
2023-05-28 01:05:34 -0300

    Respond requests with JSON

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,7 +1,7 @@
-const createTask = (req, res) => res.send("Create task");
-const getTask = (req, res) => res.send("Get task");
+const createTask = (req, res) => res.json(req.body);
+const getTask = (req, res) => res.json({ id: req.params.id });
 const getAllTasks = (req, res) => res.send("Get all tasks");
-const updateTask = (req, res) => res.send("Update task");
-const deleteTask = (req, res) => res.send("Delete task");
+const updateTask = (req, res) => res.json({ id: req.params.id, updated: req.body.updated });
+const deleteTask = (req, res) => res.json({ id: req.params.id, deleted: true });
 
 module.exports = { createTask, getTask, getAllTasks, updateTask, deleteTask };
