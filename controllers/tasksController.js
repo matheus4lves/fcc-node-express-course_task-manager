e78728a13b3cb7c557f1206c7b61366444d00248
Matheus Alves <matheusalves.bmd@gmail.com>
2023-06-03 19:53:37 -0300

    Handle get all tasks operations

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -13,8 +13,22 @@ const createTask = async (req, res) => {
     res.status(500).json({ error: message });
   }
 };
+
 const getTask = (req, res) => res.json({ id: req.params.id });
-const getAllTasks = (req, res) => res.send("Get all tasks");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(201).json({ tasks });
+  } catch (err) {
+    const {
+      errors: {
+        name: { message },
+      },
+    } = err;
+    res.status(500).json({ error: message });
+  }
+};
 const updateTask = (req, res) => res.json({ id: req.params.id, updated: req.body.updated });
 const deleteTask = (req, res) => res.json({ id: req.params.id, deleted: true });
 
