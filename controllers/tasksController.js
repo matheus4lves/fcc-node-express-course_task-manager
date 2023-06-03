61b02141d1ad375536b20333c1a15d611a1951b6
Matheus Alves <matheusalves.bmd@gmail.com>
2023-06-03 19:08:29 -0300

    Handle task creation operations

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,4 +1,18 @@
-const createTask = (req, res) => res.json(req.body);
+const Task = require("../models/Task");
+
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (err) {
+    const {
+      errors: {
+        name: { message },
+      },
+    } = err;
+    res.status(500).json({ error: message });
+  }
+};
 const getTask = (req, res) => res.json({ id: req.params.id });
 const getAllTasks = (req, res) => res.send("Get all tasks");
 const updateTask = (req, res) => res.json({ id: req.params.id, updated: req.body.updated });
