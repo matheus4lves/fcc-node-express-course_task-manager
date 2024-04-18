816e44f224b4ae95dcb7918e005ee28660ecbda6
Matheus Alves <matheusalves.bmd@gmail.com>
2024-04-18 03:43:45 -0300

    Consume dotenv types

--- app/app.cts
+++ app/app.cts
@@ -1,4 +1,5 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
--- build/app.cjs
+++ build/app.cjs
@@ -12,7 +12,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
